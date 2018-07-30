35cbfc8d2819d50a51ba8055ed1951b990b56d68
Randall <takmo@bitwisehero.com>
2018-07-30 11:30:17 -0700

    Added skills/approaches and can switch between them.

--- board-buttons/character.js
+++ board-buttons/character.js
@@ -1,6 +1,8 @@
 /* global Handlebars */
 
 var generalInfo = document.getElementById("general-info")
+var coreSkills = document.getElementById("core-skills")
+var faeSkills = document.getElementById("fae-skills")
 
 generalInfo.innerHTML += Handlebars.templates.textbox({
     'id':           'name',
@@ -8,21 +10,18 @@ generalInfo.innerHTML += Handlebars.templates.textbox({
     'placeholder':  'e.g.: Batman, Bobanthy, Kob???',
     'description':  'A good name makes for a great character!'
 });
-
 generalInfo.innerHTML += Handlebars.templates.textbox({
     'id':           'high-concept',
     'label':        'High Concept',
     'placeholder':  'Eccentric Billionaire by Day, Masked Vigillante by Night',
     'description':  'If you had to describe your character in a single phrase, what would it be?'
 });
-
 generalInfo.innerHTML += Handlebars.templates.textbox({
     'id':           'trouble',
     'label':        'Trouble',
     'placeholder':  'Cannot Pull the Trigger / Will Not Kill',
     'description':  'What keeps getting your character into trouble?'
 });
-
 generalInfo.innerHTML += Handlebars.templates.textbox({
     'id':           'aspect-1',
     'label':        'Additional Aspect',
@@ -31,7 +30,6 @@ generalInfo.innerHTML += Handlebars.templates.textbox({
                     '(PRO TIP: what aspect about your character proved' +
                     ' useful during one of their earlier adventures alone?)'
 });
-
 generalInfo.innerHTML += Handlebars.templates.textbox({
     'id':           'aspect-2',
     'label':        'Additional Aspect',
@@ -41,7 +39,6 @@ generalInfo.innerHTML += Handlebars.templates.textbox({
                     ' useful during one of their earlier adventures with' +
                     ' the player/character to your left?)'
 });
-
 generalInfo.innerHTML += Handlebars.templates.textbox({
     'id':           'aspect-3',
     'label':        'Additional Aspect',
@@ -51,7 +48,6 @@ generalInfo.innerHTML += Handlebars.templates.textbox({
                     ' useful during one of their earlier adventures with' +
                     ' the player/character to your right?)'
 });
-
 generalInfo.innerHTML += Handlebars.templates.textbox({
     'id':           'stunt-1',
     'label':        'Stunt 1',
@@ -59,7 +55,6 @@ generalInfo.innerHTML += Handlebars.templates.textbox({
     'description':  'What can your character do with a skill/approach ' +
                     'better than everyone else?'
 });
-
 generalInfo.innerHTML += Handlebars.templates.textbox({
     'id':           'stunt-2',
     'label':        'Stunt 2',
@@ -67,7 +62,6 @@ generalInfo.innerHTML += Handlebars.templates.textbox({
     'description':  'What can your character do with a skill/approach ' +
                     'better than everyone else?'
 });
-
 generalInfo.innerHTML += Handlebars.templates.textbox({
     'id':           'stunt-3',
     'label':        'Stunt 3',
@@ -75,3 +69,39 @@ generalInfo.innerHTML += Handlebars.templates.textbox({
     'description':  'What can your character do with a skill/approach ' +
                     'better than everyone else?'
 });
+
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Athletics'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Burglary'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Contacts'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Crafts'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Deceive'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Drive'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Empathy'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Fight'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Investigate'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Lore'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Notice'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Physique'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Provoke'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Rapport'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Resources'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Shoot'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Stealth'});
+coreSkills.innerHTML += Handlebars.templates.skill({'id': 'Will'});
+
+faeSkills.innerHTML += Handlebars.templates.skill({'id': 'Careful'});
+faeSkills.innerHTML += Handlebars.templates.skill({'id': 'Clever'});
+faeSkills.innerHTML += Handlebars.templates.skill({'id': 'Flashy'});
+faeSkills.innerHTML += Handlebars.templates.skill({'id': 'Forceful'});
+faeSkills.innerHTML += Handlebars.templates.skill({'id': 'Quick'});
+faeSkills.innerHTML += Handlebars.templates.skill({'id': 'Sneaky'});
+
+function switchAccelerated(){
+    document.getElementById('core-skills').style = "display: none";
+    document.getElementById('fae-skills').style = "";
+}
+
+function switchCore(){
+    document.getElementById('fae-skills').style = "display: none";
+    document.getElementById('core-skills').style = "";
+}
--- template-functions.js
+++ template-functions.js
@@ -1,5 +1,21 @@
 var currentSelection = '';
 
+function onSkillSelect(id, button){
+    // reset button states
+    [0, 1, 2, 3, 4].map(function(num){
+        let elem = document.getElementById('skill-' + id + '-' + num);
+        elem.className = '';
+    });
+
+    // update the selected button state
+    let elem = document.getElementById('skill-' + id + '-' + button);
+    elem.className = 'mod-primary';
+
+    // update the hidden text
+    let skill = document.getElementById('skill-' + id);
+    skill.value = elem.innerHTML;
+}
+
 function onTextboxFocus(id){
     if(currentSelection !== '' && currentSelection !== id){
         let lastSection = document.getElementById(currentSelection + '-hidden');
