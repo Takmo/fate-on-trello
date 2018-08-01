1f0004a091d318fb333ab6cc19976a18066632b7
Randall <takmo@bitwisehero.com>
2018-07-31 17:51:52 -0700

    Moved templates more locally.

--- board-buttons/template-functions.js
+++ board-buttons/template-functions.js
@@ -1,1 +1,27 @@
-../template-functions.js
+var currentSelection = '';
+
+function onSkillSelect(id, button){
+    // reset button states
+    [0, 1, 2, 3, 4].map(function(num){
+        let elem = document.getElementById('skill-' + id + '-' + num);
+        elem.className = '';
+    });
+
+    // update the selected button state
+    let elem = document.getElementById('skill-' + id + '-' + button);
+    elem.className = 'mod-primary';
+
+    // update the hidden text
+    let skill = document.getElementById('skill-' + id);
+    skill.value = elem.innerHTML;
+}
+
+function onTextboxFocus(id){
+    if(currentSelection !== '' && currentSelection !== id){
+        let lastSection = document.getElementById(currentSelection + '-hidden');
+        lastSection.style = "display: none";
+    }
+    let newSection = document.getElementById(id + '-hidden');
+    newSection.style = "display: block";
+    currentSelection = id;
+}
